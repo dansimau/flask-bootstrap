4d8697e17fd72471e45a5ef91351752299bfd4b4
Daniel Simmons <dan@dans.im>
2012-05-24 20:49:46 +0100

    Add Twitter Bootstrap and basic template placeholder.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_pyfile('%s/config/default.cfg' % app.root_path)
 app.config.from_envvar('FLASK_CONFIG')
+
+@app.route('/')
+def hello():
+    return render_template('base.html')
