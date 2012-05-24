5d18b7711204f842075dc990dc553c98750f99b4
Daniel Simmons <dan@dans.im>
2012-05-24 20:42:52 +0100

    Change alembic.ini file path

--- fabfile/db.py
+++ fabfile/db.py
@@ -3,9 +3,11 @@ from fabric.context_managers import settings, hide
 from fabric.colors import cyan
 from utils import do
 
+config_file_path = 'db/alembic.ini'
+
 @task
 def build():
     """Initialise and migrate database to latest version."""
     print(cyan('\nUpdating database...'))
     with settings(hide('warnings'), warn_only=True):
-        do('venv/bin/alembic init db/postgresql')
+        do('venv/bin/alembic -c %s init db/postgresql' % config_file_path)
